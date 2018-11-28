bd9053e30116c81404e4f5f3e6b190094369adde
ninamalmqvist <43323722+ninamalmqvist@users.noreply.github.com>
2018-11-28 11:08:48 +0100

    Add files via upload

--- Lab2/src/PriorityQueue.java
+++ Lab2/src/PriorityQueue.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class PriorityQueue<E> {
 
 	private ArrayList<E> heap = new ArrayList<E>();
 	private Comparator<E> comparator;
+	private HashMap<E, Integer> hashMap = new HashMap();
 
 	public PriorityQueue(Comparator<E> comparator) {
 		this.comparator = comparator;
@@ -18,8 +20,11 @@ public class PriorityQueue<E> {
 
 	// Adds an item to the priority queue.
 	public void add(E x){
+		assert invariant() : showHeap();
 		heap.add(x);
+		hashMap.put(x,heap.size()-1);
 		siftUp(heap.size()); 
+		assert invariant() : showHeap();
 	}
 
 	// Returns the smallest item in the priority queue.
@@ -34,34 +39,44 @@ public class PriorityQueue<E> {
 	// Removes the smallest item in the priority queue.
 	// Throws NoSuchElementException if empty.
 	public void deleteMinimum() {
+		assert invariant() : showHeap();
 		if (size() == 0)
 			throw new NoSuchElementException();
 
 		heap.set(0, heap.get(heap.size()-1));
+		hashMap.replace(heap.get(heap.size()-1), 0);
+//		System.out.println(hashMap.size());
+		hashMap.remove(heap.get(heap.size()-1));
 		heap.remove(heap.size()-1);
+		
 		if (heap.size() > 0) siftDown(0);
+		assert invariant() : showHeap();
 	}
 	
 	// update new bid
 	public void update(E x){
+		assert invariant() : showHeap();
 		Bid xBid = (Bid) x;
 		int bidIndex = indexOfName(xBid.name);
-		
 		if(bidIndex<0) return; // if there is no old bid, do nothing
 		if(heap.size() == 1){
 			deleteMinimum();
 			add(x);
 			return;
 		}
-
+//			System.out.println(""+x + heap.get(parent(bidIndex)));
 		if(comparator.compare(x,heap.get(parent(bidIndex)))<0){
 			heap.set(bidIndex, x);
+			hashMap.replace(x, bidIndex);
+			System.out.println(9);
 			siftDown(bidIndex);
 		}
-		else{
+		else{ // ändrat
 			heap.set(bidIndex, x);
+			hashMap.replace(x, bidIndex);
 			siftUp(bidIndex+1);
 		}
+		assert invariant() : showHeap();
 	}
 	
 	private int indexOfName(String x){
@@ -89,10 +104,13 @@ public class PriorityQueue<E> {
 			if(comparator.compare(value,parentValue)>0){
 				heap.set(index, parentValue);
 				heap.set(parentIndex, value);
+				hashMap.replace(parentValue, index);
+				hashMap.replace(value, parentIndex);
 			}
 			value = parentValue;
 			index = parentIndex; 
 		}
+		
 	}
 	
 	
@@ -125,11 +143,13 @@ public class PriorityQueue<E> {
 			// carry on downwards.
 			if (comparator.compare(value, childValue) < 0) {
 				heap.set(index, childValue);
+				hashMap.replace(childValue, index);
 				index = child;
 			} else break;
 		}
 
 		heap.set(index, value);
+		hashMap.replace(value, index);
 	}
 	
 	// Helper functions for calculating the children and parent of an index.
@@ -143,5 +163,39 @@ public class PriorityQueue<E> {
 
 	private final int parent(int index) {
 		return (index-1)/2;
+	}
+	/*private boolean invariant(){
+        // TODO: return true if and only if the heap invariant is true.
+	}*/
+	
+	private boolean invariant(){
+		int index = 0;
+		int left;
+		int right;
+		E parent;
+		if(heap.size() == 0) return true;
+		//System.out.println(heap.size());
+		for(int i = 0; i<heap.size(); i++) {
+			//System.out.println(i);
+			left = leftChild(index);
+			right = rightChild(index);
+			parent = heap.get(index);
+			if (left<heap.size() && !(comparator.compare(parent,heap.get(left)) == 1)) {
+				return false;
+			}
+			else if (right<heap.size() && !(comparator.compare(parent,heap.get(left)) == 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private String showHeap(){
+		//System.out.println(9);
+		String showHeap = heap.get(0).toString();
+        for(int i=1; i<heap.size(); i++){
+        	showHeap += ", " + heap.get(i).toString();
+        }
+        return showHeap;
 	}
 }
